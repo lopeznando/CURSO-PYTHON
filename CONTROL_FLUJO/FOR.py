a1fea5978af93d53e47cc2dc74dfd30abd936cc7
Nando Lopez <nandolopezsantaria@gamil.com>
2023-06-22 12:31:35 -0500

    Trabajo FOR

---
+++ CONTROL_FLUJO/FOR.py
@@ -0,0 +1,17 @@
+vocales=['a','e','i','o','u']
+for numero in range(0,5):
+   print(vocales[numero])
+
+vocales=['a','e','i','o','u']
+for vocal in vocales:
+ print(vocal)
+
+colores=['azul','naranja','negro','verde','rojo']
+for color in colores:
+    if color=='rojo':
+        print('color encontrado')
+        break
+    print(color)
+
+
+
