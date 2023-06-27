1de0691a46aeee1c1c4b9077cbe614cc18994371
Nando Lopez <nandolopezsantaria@gamil.com>
2023-06-27 09:59:59 -0500

    Trabajo append

---
+++ CONTROL_FLUJO/Append.py
@@ -0,0 +1,34 @@
+# lista=[]
+# print(lista)
+# primerDato=input('ingresa una fruta: ')
+# lista.append(primerDato)
+# print(lista)
+# segundoDato=input('ingrese la segunda fruta: ')
+# lista.append(segundoDato)
+# print(lista)
+
+# lista=[]
+# while True:
+#     pedirDato=input('ingrese el dato: ')
+#     if pedirDato=='si':
+#        break
+#     lista.append(pedirDato)
+# print(lista)
+
+lista=[]
+print (lista)
+for _ in range(0,5):
+    datoPedido=input('ingrese el dato: ')
+    lista.append(datoPedido)
+print(lista)
+
+
+
+
+
+    
+
+
+    
+
+
---
+++ CONTROL_FLUJO/Tabla_Multiplicar.py
@@ -0,0 +1,11 @@
+# tablaDe=int(input('ingresa un numero'))
+# for numero in range(1,13):
+#     resultado=numero*tablaDe
+#     print(f"{numero} * {tablaDe} = {resultado}")
+
+numero=int(input('ingresa un numero: '))
+factorial=1 
+for num in range(1,numero+1):
+    factorial=factorial*num
+print(factorial)
+   
